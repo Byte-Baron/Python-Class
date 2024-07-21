1d4907d1464f403fe3e0ae3f62e0ddd1e7c24dba
Stan <86017139+Byte-Baron@users.noreply.github.com>
2024-07-21 18:58:04 +0330

    Add files via upload

---
+++ Class.py
@@ -0,0 +1,241 @@
+#Class 1
+print("\n")
+class11=input("Enter class Name : ")
+print("\n")
+#1
+name11=input("Enter the Name of the std1 of "+class11+" class : " )
+math11=float(input("Enter the Math grade of "+name11+" student : "))
+chemistry11=float(input("Enter the Chemistry grade of "+name11+" student : "))
+physics11=float(input("Enter the Physics grade of "+name11+" student : "))
+avrg11=float(math11+chemistry11+physics11)/3
+print("\n")
+#2
+name12=input("Enter the Name of the std2 of "+class11+" class : " )
+math12=float(input("Enter the Math grade of "+name12+" student : "))
+chemistry12=float(input("Enter the Chemistry grade of "+name12+" student : "))
+physics12=float(input("Enter the Physics grade of "+name12+" student : "))
+avrg12=float(math12+chemistry12+physics12)/3
+print("\n")
+#3
+name13=input("Enter the Name of the std3 of "+class11+" class : " )
+math13=float(input("Enter the Math grade of "+name13+" student : "))
+chemistry13=float(input("Enter the Chemistry grade of "+name13+" student : "))
+physics13=float(input("Enter the Physics grade of "+name13+" student : "))
+avrg13=float(math13+chemistry13+physics13)/3
+print("\n")
+#4
+name14=input("Enter the Name of the std4 of "+class11+" class : " )
+math14=float(input("Enter the Math grade of "+name14+" student : "))
+chemistry14=float(input("Enter the Chemistry grade of "+name14+" student : "))
+physics14=float(input("Enter the Physics grade of "+name14+" student : "))
+avrg14=float(math14+chemistry14+physics14)/3
+print("\n")
+#5
+
+name15=input("Enter the Name of the std5 of "+class11+" class : " )
+math15=float(input("Enter the Math grade of "+name15+" student : "))
+chemistry15=float(input("Enter the Chemistry grade of "+name15+" student : "))
+physics15=float(input("Enter the Physics grade of "+name15+" student : "))
+avrg15=float(math15+chemistry15+physics15)/3
+print("\n")
+print("                                         NExt Class                                      ")
+
+#--------------------------------------------------------------------------------------------------#
+#Class 2
+class21=input("Enter class Name : ")
+print("\n")
+#1
+name21=input("Enter the Name of the std1 of "+class21+" class : " )
+math21=float(input("Enter the Math grade of "+name21+" student : "))
+chemistry21=float(input("Enter the Chemistry grade of "+name21+" student : "))
+physics21=float(input("Enter the Physics grade of "+name21+" student : "))
+avrg21=float(math21+chemistry21+physics21)/3
+print("\n")
+#2
+name22=input("Enter the Name of the std2 of "+class21+" class : " )
+math22=float(input("Enter the Math grade of "+name22+" student : "))
+chemistry22=float(input("Enter the Chemistry grade of "+name22+" student : "))
+physics22=float(input("Enter the Physics grade of "+name22+" student : "))
+avrg22=float(math22+chemistry22+physics22)/3
+#3
+name23=input("Enter the Name of the std3 of "+class21+" class : " )
+math23=float(input("Enter the Math grade of "+name23+" student : "))
+chemistry23=float(input("Enter the Chemistry grade of "+name23+" student : "))
+physics23=float(input("Enter the Physics grade of "+name23+" student : "))
+avrg23=float(math23+chemistry23+physics23)/3
+print("\n")
+#4
+name24=input("Enter the Name of the std4 of "+class21+" class : " )
+math24=float(input("Enter the Math grade of "+name24+" student : "))
+chemistry24=float(input("Enter the Chemistry grade of "+name24+" student : "))
+physics24=float(input("Enter the Physics grade of "+name24+" student : "))
+avrg24=float(math24+chemistry24+physics24)/3
+print("\n")
+#5
+name25=input("Enter the Name of the std5 of "+class21+" class : " )
+math25=float(input("Enter the Math grade of "+name25+" student : "))
+chemistry25=float(input("Enter the Chemistry grade of "+name25+" student : "))
+physics25=float(input("Enter the Physics grade of "+name25+" student : "))
+avrg25=float(math25+chemistry25+physics25)/3
+print("\n")
+print("                                         NExt Class                                      ")
+
+#--------------------------------------------------------------------------------------------------#
+#class 3
+class31=input("Enter class Name : ")
+print("\n")
+#1
+name31=input("Enter the Name of the std1 of "+class31+" class : " )
+math31=float(input("Enter the Math grade of "+name31+" student : "))
+chemistry31=float(input("Enter the Chemistry grade of "+name31+" student : "))
+physics31=float(input("Enter the Physics grade of "+name31+" student : "))
+avrg31=float(math31+chemistry31+physics31)/3
+print("\n")
+#2
+name32=input("Enter the Name of the std2 of "+class31+" class : " )
+math32=float(input("Enter the Math grade of "+name32+" student : "))
+chemistry32=float(input("Enter the Chemistry grade of "+name32+" student : "))
+physics32=float(input("Enter the Physics grade of "+name32+" student : "))
+avrg32=float(math32+chemistry32+physics32)/3
+print("\n")
+#3
+name33=input("Enter the Name of the std3 of "+class31+" class : " )
+math33=float(input("Enter the Math grade of "+name33+" student : "))
+chemistry33=float(input("Enter the Chemistry grade of "+name33+" student : "))
+physics33=float(input("Enter the Physics grade of "+name33+" student : "))
+avrg33=float(math33+chemistry33+physics33)/3
+print("\n")
+#4
+name34=input("Enter the Name of the std4 of "+class31+" class : " )
+math34=float(input("Enter the Math grade of "+name34+" student : "))
+chemistry34=float(input("Enter the Chemistry grade of "+name34+" student : "))
+physics34=float(input("Enter the Physics grade of "+name34+" student : "))
+avrg34=float(math34+chemistry34+physics34)/3
+print("\n")
+#5
+name35=input("Enter the Name of the std5 of "+class31+" class : " )
+math35=float(input("Enter the Math grade of "+name35+" student : "))
+chemistry35=float(input("Enter the Chemistry grade of "+name35+" student : "))
+physics35=float(input("Enter the Physics grade of "+name35+" student : "))
+avrg35=float(math35+chemistry35+physics35)/3
+
+#--------------------------------------------------------------------------------------------------#
+# Clear the page
+print("\n" * 40)
+
+#Print section
+
+'''
+print("Class: ", class11," Student: ",name11," Math grade: ",math11," Chemistry grade : "
+      ,chemistry11," Physics grade: ",physics11," And avrage Score of the student is: ",avrg)
+'''
+
+print("-----------------------------------------------------------------------------------------------")
+print("                                   Students Informations                                       ")
+print("-----------------------------------------------------------------------------------------------")
+print("                                    Class: ",class11)
+print("                                    Name: ",name11)
+print("                                    Math Grade: ",math11)
+print("                                    Chemistry Grade: ",chemistry11)
+print("                                    Physics Grade: ",physics11)
+print("                                    Average Grade: ",avrg11)
+print("\n")
+print("                                    Class: ",class11)
+print("                                    Name: ",name12)
+print("                                    Math Grade: ",math12)
+print("                                    Chemistry Grade: ",chemistry12)
+print("                                    Physics Grade: ",physics12)
+print("                                    Average Grade: ",avrg12)
+print("\n")
+print("                                    Class: ",class11)
+print("                                    Name: ",name13)
+print("                                    Math Grade: ",math13)
+print("                                    Chemistry Grade: ",chemistry13)
+print("                                    Physics Grade: ",physics13)
+print("                                    Average Grade: ",avrg13)
+print("\n")
+print("                                    Class: ",class11)
+print("                                    Name: ",name14)
+print("                                    Math Grade: ",math14)
+print("                                    Chemistry Grade: ",chemistry14)
+print("                                    Physics Grade: ",physics14)
+print("                                    Average Grade: ",avrg14)
+print("\n")
+print("                                    Class: ",class11)
+print("                                    Name: ",name15)
+print("                                    Math Grade: ",math15)
+print("                                    Chemistry Grade: ",chemistry15)
+print("                                    Physics Grade: ",physics15)
+print("                                    Average Grade: ",avrg15)
+print("-----------------------------------------------------------------------------------------------")
+print("                                    Class: ",class21)
+print("                                    Name: ",name21)
+print("                                    Math Grade: ",math21)
+print("                                    Chemistry Grade: ",chemistry21)
+print("                                    Physics Grade: ",physics21)
+print("                                    Average Grade: ",avrg21)
+print("\n")
+print("                                    Class: ",class21)
+print("                                    Name: ",name22)
+print("                                    Math Grade: ",math22)
+print("                                    Chemistry Grade: ",chemistry22)
+print("                                    Physics Grade: ",physics22)
+print("                                    Average Grade: ",avrg22)
+print("\n")
+print("                                    Class: ",class21)
+print("                                    Name: ",name23)
+print("                                    Math Grade: ",math23)
+print("                                    Chemistry Grade: ",chemistry23)
+print("                                    Physics Grade: ",physics23)
+print("                                    Average Grade: ",avrg23)
+print("\n")
+print("                                    Class: ",class21)
+print("                                    Name: ",name24)
+print("                                    Math Grade: ",math24)
+print("                                    Chemistry Grade: ",chemistry24)
+print("                                    Physics Grade: ",physics24)
+print("                                    Average Grade: ",avrg24)
+print("\n")
+print("                                    Class: ",class21)
+print("                                    Name: ",name25)
+print("                                    Math Grade: ",math25)
+print("                                    Chemistry Grade: ",chemistry25)
+print("                                    Physics Grade: ",physics25)
+print("                                    Average Grade: ",avrg25)
+print("-----------------------------------------------------------------------------------------------")
+print("                                    Class: ",class31)
+print("                                    Name: ",name31)
+print("                                    Math Grade: ",math31)
+print("                                    Chemistry Grade: ",chemistry31)
+print("                                    Physics Grade: ",physics31)
+print("                                    Average Grade: ",avrg31)
+print("\n")
+print("                                    Class: ",class31)
+print("                                    Name: ",name32)
+print("                                    Math Grade: ",math32)
+print("                                    Chemistry Grade: ",chemistry32)
+print("                                    Physics Grade: ",physics32)
+print("                                    Average Grade: ",avrg32)
+print("\n")
+print("                                    Class: ",class31)
+print("                                    Name: ",name33)
+print("                                    Math Grade: ",math33)
+print("                                    Chemistry Grade: ",chemistry33)
+print("                                    Physics Grade: ",physics33)
+print("                                    Average Grade: ",avrg33)
+print("\n")
+print("                                    Class: ",class31)
+print("                                    Name: ",name34)
+print("                                    Math Grade: ",math34)
+print("                                    Chemistry Grade: ",chemistry34)
+print("                                    Physics Grade: ",physics34)
+print("                                    Average Grade: ",avrg34)
+print("\n")
+print("                                    Class: ",class31)
+print("                                    Name: ",name35)
+print("                                    Math Grade: ",math35)
+print("                                    Chemistry Grade: ",chemistry35)
+print("                                    Physics Grade: ",physics35)
+print("                                    Average Grade: ",avrg35)
+print("-----------------------------------------------------------------------------------------------")
+
