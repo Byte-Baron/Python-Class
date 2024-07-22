0ba2bbab55e086045381ab2c4b199dcdd16f4d92
Stan <86017139+Byte-Baron@users.noreply.github.com>
2024-07-22 23:41:27 +0330

    Add files via upload

---
+++ Elementary.class.list.py
@@ -0,0 +1,262 @@
+#Class 1
+print()
+class1 = input("Enter class Name : ")
+print("\n")
+
+std1 = []
+std2 = []
+std3 = []
+std4 = []
+std5 = []
+
+#1
+name = input("Enter the Name of the std 1" + " of " + class1 + " class : " )
+math = float(input("Enter the Math grade of " + name + " student : "))
+chemistry = float(input("Enter the Chemistry grade of " + name + " student : "))
+physics = float(input("Enter the Physics grade of " + name + " student : "))
+avrg = float(math + chemistry + physics) / 3
+std1.extend((name, math, chemistry, physics, avrg))
+#2
+name = input("Enter the Name of the std 2" + " of " + class1 + " class : " )
+math = float(input("Enter the Math grade of " + name + " student : "))
+chemistry = float(input("Enter the Chemistry grade of " + name + " student : "))
+physics = float(input("Enter the Physics grade of " + name + " student : "))
+avrg = float(math + chemistry + physics) / 3
+std2.extend((name, math, chemistry, physics, avrg))
+#3
+name = input("Enter the Name of the std 3" + " of " + class1 + " class : " )
+math = float(input("Enter the Math grade of " + name + " student : "))
+chemistry = float(input("Enter the Chemistry grade of " + name + " student : "))
+physics = float(input("Enter the Physics grade of " + name + " student : "))
+avrg = float(math + chemistry + physics) / 3
+std3.extend((name, math, chemistry, physics, avrg))
+#4
+name = input("Enter the Name of the std 4" + " of " + class1 + " class : " )
+math = float(input("Enter the Math grade of " + name + " student : "))
+chemistry = float(input("Enter the Chemistry grade of " + name + " student : "))
+physics = float(input("Enter the Physics grade of " + name + " student : "))
+avrg = float(math + chemistry + physics) / 3
+std4.extend((name, math, chemistry, physics, avrg))
+#5
+name = input("Enter the Name of the std 5" + " of " + class1 + " class : " )
+math = float(input("Enter the Math grade of " + name + " student : "))
+chemistry = float(input("Enter the Chemistry grade of " + name + " student : "))
+physics = float(input("Enter the Physics grade of " + name + " student : "))
+avrg = float(math + chemistry + physics) / 3
+std5.extend((name, math, chemistry, physics, avrg))
+print("\n")
+print("                                         NExt Class                                      ")
+
+#--------------------------------------------------------------------------------------------------#
+#Class 2
+print()
+class2 = input("Enter class Name : ")
+print("\n")
+
+std6 = []
+std7 = []
+std8 = []
+std9 = []
+std10 = []
+
+#1
+name = input("Enter the Name of the std 1" + " of " + class2 + " class : " )
+math = float(input("Enter the Math grade of " + name + " student : "))
+chemistry = float(input("Enter the Chemistry grade of " + name + " student : "))
+physics = float(input("Enter the Physics grade of " + name + " student : "))
+avrg = float(math + chemistry + physics) / 3
+std6.extend((name, math, chemistry, physics, avrg))
+#2
+name = input("Enter the Name of the std 2" + " of " + class2 + " class : " )
+math = float(input("Enter the Math grade of " + name + " student : "))
+chemistry = float(input("Enter the Chemistry grade of " + name + " student : "))
+physics = float(input("Enter the Physics grade of " + name + " student : "))
+avrg = float(math + chemistry + physics) / 3
+std7.extend((name, math, chemistry, physics, avrg))
+#3
+name = input("Enter the Name of the std 3" + " of " + class2 + " class : " )
+math = float(input("Enter the Math grade of " + name + " student : "))
+chemistry = float(input("Enter the Chemistry grade of " + name + " student : "))
+physics = float(input("Enter the Physics grade of " + name + " student : "))
+avrg = float(math + chemistry + physics) / 3
+std8.extend((name, math, chemistry, physics, avrg))
+#4
+name = input("Enter the Name of the std 4" + " of " + class2 + " class : " )
+math = float(input("Enter the Math grade of " + name + " student : "))
+chemistry = float(input("Enter the Chemistry grade of " + name + " student : "))
+physics = float(input("Enter the Physics grade of " + name + " student : "))
+avrg = float(math + chemistry + physics) / 3
+std9.extend((name, math, chemistry, physics, avrg))
+#5
+name = input("Enter the Name of the std 5" + " of " + class2 + " class : " )
+math = float(input("Enter the Math grade of " + name + " student : "))
+chemistry = float(input("Enter the Chemistry grade of " + name + " student : "))
+physics = float(input("Enter the Physics grade of " + name + " student : "))
+avrg = float(math + chemistry + physics) / 3
+std10.extend((name, math, chemistry, physics, avrg))
+print("\n")
+print("                                         NExt Class                                      ")
+
+#--------------------------------------------------------------------------------------------------#
+#Class 3
+print()
+class3 = input("Enter class Name : ")
+print("\n")
+
+std11 = []
+std12 = []
+std13 = []
+std14 = []
+std15 = []
+
+#1
+name = input("Enter the Name of the std 1" + " of " + class3 + " class : " )
+math = float(input("Enter the Math grade of " + name + " student : "))
+chemistry = float(input("Enter the Chemistry grade of " + name + " student : "))
+physics = float(input("Enter the Physics grade of " + name + " student : "))
+avrg = float(math + chemistry + physics) / 3
+std11.extend((name, math, chemistry, physics, avrg))
+#2
+name = input("Enter the Name of the std 2" + " of " + class3 + " class : " )
+math = float(input("Enter the Math grade of " + name + " student : "))
+chemistry = float(input("Enter the Chemistry grade of " + name + " student : "))
+physics = float(input("Enter the Physics grade of " + name + " student : "))
+avrg = float(math + chemistry + physics) / 3
+std12.extend((name, math, chemistry, physics, avrg))
+#3
+name = input("Enter the Name of the std 3" + " of " + class3 + " class : " )
+math = float(input("Enter the Math grade of " + name + " student : "))
+chemistry = float(input("Enter the Chemistry grade of " + name + " student : "))
+physics = float(input("Enter the Physics grade of " + name + " student : "))
+avrg = float(math + chemistry + physics) / 3
+std13.extend((name, math, chemistry, physics, avrg))
+#4
+name = input("Enter the Name of the std 4" + " of " + class3 + " class : " )
+math = float(input("Enter the Math grade of " + name + " student : "))
+chemistry = float(input("Enter the Chemistry grade of " + name + " student : "))
+physics = float(input("Enter the Physics grade of " + name + " student : "))
+avrg = float(math + chemistry + physics) / 3
+std14.extend((name, math, chemistry, physics, avrg))
+#5
+name = input("Enter the Name of the std 5" + " of " + class3 + " class : " )
+math = float(input("Enter the Math grade of " + name + " student : "))
+chemistry = float(input("Enter the Chemistry grade of " + name + " student : "))
+physics = float(input("Enter the Physics grade of " + name + " student : "))
+avrg = float(math + chemistry + physics) / 3
+std15.extend((name, math, chemistry, physics, avrg))
+
+#--------------------------------------------------------------------------------------------------#
+# Clear the page
+print("\n" * 40)
+
+#Print section
+
+print("-----------------------------------------------------------------------------------------------")
+print("                                   Students Informations                                       ")
+print("-----------------------------------------------------------------------------------------------")
+print("                                    Class: ",class1)
+print("                                    Name: ",std1[0])
+print("                                    Math Grade: ",std1[1])
+print("                                    Chemistry Grade: ",std1[2])
+print("                                    Physics Grade: ",std1[3])
+print("                                    Average Grade: ",std1[4])
+print("\n")
+print("                                    Class: ",class1)
+print("                                    Name: ",std2[0])
+print("                                    Math Grade: ",std2[1])
+print("                                    Chemistry Grade: ",std2[2])
+print("                                    Physics Grade: ",std2[3])
+print("                                    Average Grade: ",std2[4])
+print("\n")
+print("                                    Class: ",class1)
+print("                                    Name: ",std3[0])
+print("                                    Math Grade: ",std3[1])
+print("                                    Chemistry Grade: ",std3[2])
+print("                                    Physics Grade: ",std3[3])
+print("                                    Average Grade: ",std3[4])
+print("\n")
+print("                                    Class: ",class1)
+print("                                    Name: ",std4[0])
+print("                                    Math Grade: ",std4[1])
+print("                                    Chemistry Grade: ",std4[2])
+print("                                    Physics Grade: ",std4[3])
+print("                                    Average Grade: ",std4[4])
+print("\n")
+print("                                    Class: ",class1)
+print("                                    Name: ",std5[0])
+print("                                    Math Grade: ",std5[1])
+print("                                    Chemistry Grade: ",std5[2])
+print("                                    Physics Grade: ",std5[3])
+print("                                    Average Grade: ",std5[4])
+print("-----------------------------------------------------------------------------------------------")
+print("                                    Class: ",class2)
+print("                                    Name: ",std6[0])
+print("                                    Math Grade: ",std6[1])
+print("                                    Chemistry Grade: ",std6[2])
+print("                                    Physics Grade: ",std6[3])
+print("                                    Average Grade: ",std6[4])
+print("\n")
+print("                                    Class: ",class2)
+print("                                    Name: ",std7[0])
+print("                                    Math Grade: ",std7[1])
+print("                                    Chemistry Grade: ",std7[2])
+print("                                    Physics Grade: ",std7[3])
+print("                                    Average Grade: ",std7[4])
+print("\n")
+print("                                    Class: ",class2)
+print("                                    Name: ",std8[0])
+print("                                    Math Grade: ",std8[1])
+print("                                    Chemistry Grade: ",std8[2])
+print("                                    Physics Grade: ",std8[3])
+print("                                    Average Grade: ",std8[4])
+print("\n")
+print("                                    Class: ",class2)
+print("                                    Name: ",std9[0])
+print("                                    Math Grade: ",std9[1])
+print("                                    Chemistry Grade: ",std9[2])
+print("                                    Physics Grade: ",std9[3])
+print("                                    Average Grade: ",std9[4])
+print("\n")
+print("                                    Class: ",class2)
+print("                                    Name: ",std10[0])
+print("                                    Math Grade: ",std10[1])
+print("                                    Chemistry Grade: ",std10[2])
+print("                                    Physics Grade: ",std10[3])
+print("                                    Average Grade: ",std10[4])
+print("-----------------------------------------------------------------------------------------------")
+print("                                    Class: ",class3)
+print("                                    Name: ",std11[0])
+print("                                    Math Grade: ",std11[1])
+print("                                    Chemistry Grade: ",std11[2])
+print("                                    Physics Grade: ",std11[3])
+print("                                    Average Grade: ",std11[4])
+print("\n")
+print("                                    Class: ",class3)
+print("                                    Name: ",std12[0])
+print("                                    Math Grade: ",std12[1])
+print("                                    Chemistry Grade: ",std12[2])
+print("                                    Physics Grade: ",std12[3])
+print("                                    Average Grade: ",std12[4])
+print("\n")
+print("                                    Class: ",class3)
+print("                                    Name: ",std13[0])
+print("                                    Math Grade: ",std13[1])
+print("                                    Chemistry Grade: ",std13[2])
+print("                                    Physics Grade: ",std13[3])
+print("                                    Average Grade: ",std13[4])
+print("\n")
+print("                                    Class: ",class3)
+print("                                    Name: ",std14[0])
+print("                                    Math Grade: ",std14[1])
+print("                                    Chemistry Grade: ",std14[2])
+print("                                    Physics Grade: ",std14[3])
+print("                                    Average Grade: ",std14[4])
+print("\n")
+print("                                    Class: ",class3)
+print("                                    Name: ",std15[0])
+print("                                    Math Grade: ",std15[1])
+print("                                    Chemistry Grade: ",std15[2])
+print("                                    Physics Grade: ",std15[3])
+print("                                    Average Grade: ",std15[4])
+print("-----------------------------------------------------------------------------------------------")
+
